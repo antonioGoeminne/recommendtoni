28a0967cba9c41e3d02335540f6c39f277343881
antonionicolas___ <antonionicolas3003@gmail.com>
2024-02-29 13:53:41 -0300

    FEAT: visual fixes

--- src/features/searchList/api/get-film-list.jsx
+++ src/features/searchList/api/get-film-list.jsx
@@ -2,5 +2,7 @@ import axios from "axios";
 const BASE = "https://www.abandomoviez.net/includes/auto/autocomplete.php";
 
 export const getFilmList = async (query) => {
-  return await axios.get(BASE, { params: { q: query, limit: 10 } });
+  return await axios
+    .get(BASE, { params: { q: query, limit: 10 } })
+    .then((e) => e.data);
 };
--- src/screens/list/list-screen.js
+++ src/screens/list/list-screen.js
@@ -1,17 +1,18 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Input } from "../../ui/input/Input";
 import { useEffect, useState } from "react";
 import { useDebounce } from "../../hooks/use-debounce";
 import { getFilmList } from "../../features/searchList/api/get-film-list";
+import { HtmlVisualizer } from "../../features/recommendation/HtmlVisualizer";
 
 export default function ListScreen() {
   const [text, setText] = useState("");
   const [films, setFilms] = useState("");
 
-  const debounceText = useDebounce(text, 500);
+  const debounceText = useDebounce(text, 300);
 
   const fetchFilms = async () => {
-    const newFilms = await getFilmList();
+    const newFilms = await getFilmList(debounceText);
     setFilms(newFilms);
   };
 
@@ -19,16 +20,36 @@ export default function ListScreen() {
     fetchFilms();
   }, [debounceText]);
 
-  console.log("films", films);
-
   return (
-    <View className="px-4 py-10 bg-white">
-      <Text className="text-lg font-bold text-center">Buscá una peli</Text>
-      <Input
-        onChangeText={setText}
-        sx="my-10"
-        placeholder="El señor de los anillos..."
-      />
-    </View>
+    <ScrollView className='bg-white'>
+      <View className="px-4 py-10 bg-white margin">
+        <Text className="text-lg font-bold text-center">Buscá una peli</Text>
+        <Input
+          onChangeText={setText}
+          sx="my-10"
+          placeholder="El señor de los anillos..."
+        />
+        {films?.length ? (
+          <HtmlVisualizer
+            htmlContent={films}
+            tagsStyles={{
+              div: {
+                margin: 2,
+                borderRadius: 4,
+                borderBottomWidth: 1,
+                borderColor: "#ccc",
+              },
+            }}
+          />
+        ) : (
+          <View className="mx-auto">
+            <Image
+              style={{ width: 300, height: 300 }}
+              source={require("../../waiting2.png")}
+            />
+          </View>
+        )}
+      </View>
+    </ScrollView>
   );
 }
